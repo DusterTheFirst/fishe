ca8ec91e749ab318d995f5df689e3a256232bfef
Zachary Kohnen <me@dusterthefirst.com>
2023-07-05 12:45:24 +0200

    fish settings

--- src/fish.ts
+++ src/fish.ts
@@ -4,100 +4,122 @@ import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 import fish_glb from "../textures/high_detailed_fish/fish.glb";
 
 export class Fish {
-    private fish: THREE.Object3D;
-    private light: THREE.Object3D;
-    private camera: THREE.PerspectiveCamera;
-    private scene: THREE.Scene;
-    private renderer: THREE.WebGLRenderer;
+  private fish: THREE.Object3D;
+  private light: THREE.Object3D;
+  private camera: THREE.PerspectiveCamera;
+  private scene: THREE.Scene;
+  private renderer: THREE.WebGLRenderer;
 
-    private _running: boolean = false;
+  private _running: boolean = false;
 
-    get running() {
-        return this._running;
+  get running() {
+    return this._running;
+  }
+
+  private _crunchiness: number = 0.5;
+
+  get crunchiness() {
+    return this._crunchiness;
+  }
+
+  set crunchiness(crunchiness) {
+    this._crunchiness = Math.max(Math.min(crunchiness, 1.0), 0.0);
+  }
+
+  public bpm: number = 120;
+
+  private constructor(fish: THREE.Object3D, canvas: HTMLCanvasElement) {
+    this.fish = fish;
+    this.light = new THREE.AmbientLight(new THREE.Color(0xffffff), 1.0);
+    this.camera = (() => {
+      const camera = new THREE.PerspectiveCamera(
+        75,
+        canvas.clientWidth / canvas.clientHeight,
+        0.1,
+        1000
+      );
+      camera.position.set(0.0, 1.0, 5.0);
+      camera.lookAt(new THREE.Vector3(0.0, 0.0, 0.0));
+
+      return camera;
+    })();
+
+    this.scene = (() => {
+      const scene = new THREE.Scene();
+      scene.add(this.fish);
+      scene.add(this.light);
+
+      return scene;
+    })();
+
+    this.renderer = (() => {
+      const renderer = new THREE.WebGLRenderer({
+        canvas: canvas,
+        antialias: true,
+      });
+      renderer.setClearColor(new THREE.Color(0xffffff));
+
+      return renderer;
+    })();
+  }
+
+  public static async load(canvas: HTMLCanvasElement) {
+    const loader = new GLTFLoader();
+    const fish_model = await loader.loadAsync(fish_glb);
+    const fish = fish_model.scene.getObjectByName("fish")!;
+
+    return new Fish(fish, canvas);
+  }
+
+  public start() {
+    if (!this._running) {
+      requestAnimationFrame(this.animate);
+
+      this._running = true;
+      return true;
     }
 
-    private constructor(fish: THREE.Object3D, canvas: HTMLCanvasElement) {
-        this.fish = fish;
-        this.light = new THREE.AmbientLight(new THREE.Color(0xffffff), 1.0);
-        this.camera = (() => {
-            const camera = new THREE.PerspectiveCamera(
-                75,
-                canvas.clientWidth / canvas.clientHeight,
-                0.1,
-                1000
-            );
-            camera.position.set(0.0, 5.0, 5.0);
-            camera.lookAt(new THREE.Vector3(0.0, 0.0, 0.0));
+    return false;
+  }
 
-            return camera;
-        })();
+  public stop() {
+    const previous_running = this._running;
 
-        this.scene = (() => {
-            const scene = new THREE.Scene();
-            scene.add(this.fish);
-            scene.add(this.light);
+    this._running = false;
 
-            return scene;
-        })();
+    return previous_running;
+  }
 
-        this.renderer = (() => {
-            const renderer = new THREE.WebGLRenderer({
-                canvas: canvas,
-                antialias: true,
-            });
-            renderer.setClearColor(new THREE.Color(0xffffff));
+  private get animate() {
+    return this._animate.bind(this);
+  }
 
-            return renderer;
-        })();
+  private _animate(milliseconds: DOMHighResTimeStamp) {
+    const seconds = milliseconds * 0.001;
+
+    this.fish.rotation.y =
+      (-Math.PI * 2 * (this.bpm / 60) * seconds) % (Math.PI * 2);
+
+    // Actually do the render
+    const canvas = this.renderer.domElement;
+    const width = canvas.clientWidth;
+    const height = canvas.clientHeight;
+
+    this.camera.aspect = width / height;
+    this.camera.updateProjectionMatrix();
+
+    const crunchy_width = width * (1 - this.crunchiness);
+    const crunchy_height = height * (1 - this.crunchiness);
+    const needResize =
+      canvas.width !== crunchy_width || canvas.height !== crunchy_height;
+    if (needResize) {
+      this.renderer.setSize(crunchy_width, crunchy_height, false);
     }
 
-    public static async load(canvas: HTMLCanvasElement) {
-        const loader = new GLTFLoader();
-        const fish_model = await loader.loadAsync(fish_glb);
-        const fish = fish_model.scene.getObjectByName("fish")!;
+    this.renderer.render(this.scene, this.camera);
 
-        return new Fish(fish, canvas);
+    if (this._running) {
+      requestAnimationFrame(this.animate);
     }
-
-    public start() {
-        if (!this._running) {
-            requestAnimationFrame(this.animate);
-
-            this._running = true;
-            return true;
-        }
-
-        return false;
-    }
-
-    public stop() {
-        const previous_running = this._running;
-
-        this._running = false;
-
-        return previous_running;
-    }
-
-    private get animate() {
-        return this._animate.bind(this);
-    }
-
-    private _animate(milliseconds: DOMHighResTimeStamp) {
-        const seconds = milliseconds * 0.001;
-        void seconds; // FIXME: actually use delta time
-        // console.dir(seconds);
-
-        this.fish.rotateY(Math.PI / 48.0);
-
-        this.camera.aspect =
-            this.renderer.domElement.clientWidth /
-            this.renderer.domElement.clientHeight;
-        this.camera.updateProjectionMatrix();
-
-        this.renderer.render(this.scene, this.camera);
-
-        if (this._running) {
-            requestAnimationFrame(this.animate);
-        }
-    }
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,44 @@
 window.addEventListener("DOMContentLoaded", () => {
-    const fish_button =
-        document.querySelector<HTMLButtonElement>("button#fish_button")!;
-    const fish_canvas = document.querySelector("canvas")!;
+  const fish_button =
+    document.querySelector<HTMLButtonElement>("button#fish_button")!;
+  const fish_canvas = document.querySelector("canvas")!;
+  const crunch_slider =
+    document.querySelector<HTMLInputElement>("input#crunch")!;
+  const bpm_input = document.querySelector<HTMLInputElement>("input#bpm")!;
 
-    let fish: import("./fish").Fish | null = null;
+  let fish: import("./fish").Fish | null = null;
 
-    fish_button.addEventListener(
-        "click",
-        async () => {
-            const { Fish } = await import("./fish");
+  crunch_slider.addEventListener("input", () => {
+    if (fish !== null) {
+      fish.crunchiness = crunch_slider.valueAsNumber / 100;
 
-            if (fish === null) {
-                fish = await Fish.load(fish_canvas);
-            }
+      console.dir(fish.crunchiness);
+    }
+  });
 
-            console.dir(fish.running);
+  bpm_input.addEventListener("change", () => {
+    if (fish !== null) {
+      fish.bpm = bpm_input.valueAsNumber;
 
-            if (fish.running) {
-                fish.stop();
-            } else {
-                fish.start();
-            }
-        },
-        { once: false }
-    );
+      console.dir(fish.bpm);
+    }
+  });
+
+  fish_button.addEventListener("click", async () => {
+    const { Fish } = await import("./fish");
+
+    if (fish === null) {
+      fish = await Fish.load(fish_canvas);
+      fish.crunchiness = crunch_slider.valueAsNumber / 100;
+      fish.bpm = bpm_input.valueAsNumber;
+    }
+
+    console.dir(fish.running);
+
+    if (fish.running) {
+      fish.stop();
+    } else {
+      fish.start();
+    }
+  });
 });
